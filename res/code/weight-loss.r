bd92783a7912420868698168d526daff20a5d6f7
Sergio Garcia Prado <sergio@garciparedes.me>
2018-12-31 12:35:20 +0100

    Starting to work on differences

--- res/code/weight-loss.r
+++ res/code/weight-loss.r
@@ -112,14 +112,15 @@ weightloss <- read.csv(paste0(BASE_DATA_PATH, 'weight-loss.csv'))
 weightloss$Month <- ymd(weightloss$Month, truncated = 2)
 colnames(weightloss) <- c("index", "values")
 
-var(weightloss$values)
+variances <- data.frame()
+variances <- rbind(variances, list(name="Base", variance=var(weightloss$values)), stringsAsFactors = FALSE)
 PlotTimeSeries(weightloss, seasonality = 12, lags = 84) %>%
   { save_plot(paste0(BASE_IMG_PATH, 'weightloss.png'), .,
              base_aspect_ratio = 1, base_height = 12) }
 
 
 values <- diff(weightloss$values, 1, 1)
-var(values)
+variances <- rbind(variances, list(name="diff-1", variance=var(values)), stringsAsFactors = FALSE)
 df <- data.frame(index = 1:length(values), values=values)
 PlotTimeSeries(df, seasonality = 12, lags = 84) %>%
  { save_plot(paste0(BASE_IMG_PATH, 'weightloss-diff-1.png'), .,
@@ -127,7 +128,7 @@ PlotTimeSeries(df, seasonality = 12, lags = 84) %>%
 
 
 values <- diff(weightloss$values, 12, 1)
-var(values)
+variances <- rbind(variances, list(name="diff-12", variance=var(values)), stringsAsFactors = FALSE)
 df <- data.frame(index = 1:length(values), values=values)
 PlotTimeSeries(df, seasonality = 12, lags = 84) %>%
  { save_plot(paste0(BASE_IMG_PATH, 'weightloss-diff-12.png'), .,
@@ -135,14 +136,14 @@ PlotTimeSeries(df, seasonality = 12, lags = 84) %>%
 
 
 values <- diff(weightloss$values, 12, 2)
-var(values)
+variances <- rbind(variances, list(name="diff-12-12", variance=var(values)), stringsAsFactors = FALSE)
 df <- data.frame(index = 1:length(values), values=values)
 PlotTimeSeries(df, seasonality = 12, lags = 84) %>%
  { save_plot(paste0(BASE_IMG_PATH, 'weightloss-diff-12-12.png'), .,
             base_aspect_ratio = 1, base_height = 12) }
 
 values <- diff(diff(weightloss$values, 1, 1), 12, 1)
-var(values)
+variances <- rbind(variances, list(name="diff-1-12", variance=var(values)), stringsAsFactors = FALSE)
 df <- data.frame(index = 1:length(values), values=values)
 PlotTimeSeries(df, seasonality = 12, lags = 84) %>%
  { save_plot(paste0(BASE_IMG_PATH, 'weightloss-diff-1-12.png'), .,
@@ -150,14 +151,14 @@ PlotTimeSeries(df, seasonality = 12, lags = 84) %>%
 
 
 values <- diff(diff(weightloss$values, 1, 1), 12, 2)
-var(values)
+variances <- rbind(variances, list(name="diff-1-12-12", variance=var(values)), stringsAsFactors = FALSE)
 df <- data.frame(index = 1:length(values), values=values)
 PlotTimeSeries(df, seasonality = 12, lags = 84) %>%
   { save_plot(paste0(BASE_IMG_PATH, 'weightloss-diff-1-12-12.png'), .,
             base_aspect_ratio = 1, base_height = 12) }
 
 values <- diff(diff(weightloss$values, 1, 2), 12, 1)
-var(values)
+variances <- rbind(variances, list(name="diff-1-1-12", variance=var(values)), stringsAsFactors = FALSE)
 df <- data.frame(index = 1:length(values), values=values)
 PlotTimeSeries(df, seasonality = 12, lags = 84) %>%
  { save_plot(paste0(BASE_IMG_PATH, 'weightloss-diff-1-1-12.png'), .,
@@ -165,7 +166,11 @@ PlotTimeSeries(df, seasonality = 12, lags = 84) %>%
 
 
 values <- diff(diff(weightloss$values, 1, 2), 12, 2)
+variances <- rbind(variances, list(name="diff-1-1-12-12", variance=var(values)), stringsAsFactors = FALSE)
 df <- data.frame(index = 1:length(values), values=values)
 PlotTimeSeries(df, seasonality = 12, lags = 84) %>%
  { save_plot(paste0(BASE_IMG_PATH, 'weightloss-diff-1-1-12-12.png'), .,
             base_aspect_ratio = 1, base_height = 12) }
+
+variances$variance <- round(variances$variance, digits = 2)
+write.csv(variances, paste0(BASE_DATA_PATH, 'variance-differences.csv'), row.names = FALSE, quote = FALSE)
